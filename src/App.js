06fc0fe12ef30d2aca18a708352276a7a26b95e6
Naim-Ahmed-Mamun <naimahmod852@gmail.com>
2021-10-26 18:38:56 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import TeamMembers from './components/Team-Members/TeamMembers';
+import Update from './components/Update/Update';
 
 function App() {
   return (
     <>
-     <Header></Header>
-     <TeamMembers></TeamMembers>
+
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <TeamMembers></TeamMembers>
+          </Route>
+          <Route path="/update/:id">
+              <Update></Update>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/Team-Members/TeamMembers.js
+++ src/components/Team-Members/TeamMembers.js
@@ -3,42 +3,61 @@ import Member from '../Member/Member';
 import SelectedMembers from '../SelectedMembers/SelectedMembers';
 
 const TeamMembers = () => {
-    // set members in state
-    const [members, setMembers] = useState([]);
-    // selected members set in state
-    const [selectedMembers,setSelectedMembers] = useState([]);
-    // load data
-    useEffect(() => {
-        fetch('/data.JSON')
-            .then(res => res.json())
-            .then(data => setMembers(data))
-    }, []);
-    // update selected members
-    const handleMembersDetails = (member) => {
-        const newMember = [...selectedMembers,member]
-        setSelectedMembers(newMember);
-    }
-    return (
-        <>
-            <div className="container mt-5">
-                    <div className="row">
-                        <div className="col-lg-9">
-                            <div className="row">
-                                {
-                                    members.map(member => <Member key={member.id} handleMembersDetails={handleMembersDetails}
-                                    member={member}></Member>)
-                                }
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                            <div className="selected_members">
-                                <SelectedMembers selectedMembers={selectedMembers}></SelectedMembers>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-        </>
-    );
+	// set members in state
+	const [members, setMembers] = useState([]);
+	// selected members set in state
+	const [selectedMembers, setSelectedMembers] = useState([]);
+
+	// load data
+	useEffect(() => {
+		fetch('http://localhost:4000/singers')
+			.then(res => res.json())
+			.then(data => setMembers(data))
+	}, []);
+	// update selected members
+	const handleMembersDetails = (member) => {
+		// console.log(member);
+		// const newMember = [...selectedMembers, member]
+		// setSelectedMembers(newMember);
+		// useEffect(() => {
+			fetch('http://localhost:4000/addedMember',{
+			method:'POST',
+			headers:{
+				'content-type': 'application/json'
+			},
+			body:JSON.stringify(member)
+		})
+		.then(res => res.json())
+		.then(data => {
+			if(data.acknowledged){
+				alert('Added successfully')
+			}
+			setSelectedMembers(data)
+		})
+		// },[member])
+	};
+	console.log(selectedMembers);
+	return (
+		<>
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col-lg-9">
+						<div className="row">
+							{
+								members.map(member => <Member key={member.id} handleMembersDetails={handleMembersDetails}
+									member={member}></Member>)
+							}
+						</div>
+					</div>
+					<div className="col-lg-3">
+						<div className="selected_members">
+							<SelectedMembers selectedMembers={selectedMembers}></SelectedMembers>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default TeamMembers;
